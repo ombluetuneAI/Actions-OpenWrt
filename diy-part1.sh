f3f3e50e1968f875434b86236027fc6fefabe135
txb <1571314213@qq.com>
2022-10-29 12:55:06 +0800

    增加har-20s2u1配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,10 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# add thems
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+
+# Add a feed source
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default #passwall
