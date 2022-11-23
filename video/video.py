af9e3eb07772da33744ea127c9dde32b2e1d2956
txb <1571314213@qq.com>
2022-11-24 00:22:31 +0800

    1

--- video/video.py
+++ video/video.py
@@ -11,6 +11,7 @@ import json
 import logging
 import csv
 
+requests.packages.urllib3.disable_warnings()
 
 # 越多越好
 meizi_headers = [
@@ -35,8 +36,8 @@ spkb = "https://spankbang.com"
 web_info_path = "web_info.csv"
 video_info_path = "video_info.csv"
 exist_id_path = "exist_id.csv"
-related_playlists_path = "related.csv"
-related_playlists_path_full = "related_full.csv"
+related_album_path = "album.csv"
+related_album_full_path = "album_full.csv"
 
 global headers
 headers = {
@@ -99,7 +100,7 @@ def web_requests(url):
 """
 def parse_related_playlists(data):
     try:
-        local = read_csv(related_playlists_path)
+        local = read_csv(related_album_path)
         urls = []
         for a in local:
             urls.append(a[0])
@@ -113,8 +114,8 @@ def parse_related_playlists(data):
             if (url not in urls):
                 new.append([url])
                 new_full.append([url, name])
-        write_csv(related_playlists_path, new, "a+")
-        write_csv(related_playlists_path_full, new_full, "a+")
+        write_csv(related_album_path, new, "a+")
+        write_csv(related_album_full_path, new_full, "a+")
     except:
         logging.info("parse_related_playlists err")
 
@@ -153,27 +154,6 @@ def get_dl_links():
 
 
 """
-从video page抓取video info
-"""
-def parse_html(data):
-    local_info = read_csv(web_info_path)
-    all_id = []  # 获取本地所有的id
-    for a in local_info:
-        all_id.append(a[0])
-    new_info = []
-    soup = BeautifulSoup(data, "html.parser")
-    video_lists = soup.find("div", class_="video-list video-rotate video-list-with-ads").find_all("div", class_="video-item")
-    for video_list in video_lists:
-        url_a = video_list.find("a", class_="n")
-        if (url_a != None):
-            url = spkb + url_a["href"]
-            id = video_list["data-id"]
-            if (id not in all_id):
-                new_info.append([id, url])
-    write_csv(web_info_path, new_info, "a+")
-
-
-"""
 读取本地共享列表url
 """
 def get_shared_playlists():
@@ -187,29 +167,78 @@ def get_shared_playlists():
     f.close()
     return shared_playlists
 
+def album_parse_html(data):
+    info = []
+    is_last_page = False
+    soup = BeautifulSoup(data, "html.parser")
+    video_lists = soup.find("div", class_="video-list video-rotate video-list-with-ads").find_all("div", class_="video-item")
+    for video_list in video_lists:
+        url_a = video_list.find("a", class_="n")
+        if (url_a != None):
+            url = spkb + url_a["href"]
+            id = video_list["data-id"]
+            info.append([id, url])
+    return info
+
+def album_get_all_items(url):
+    page = 1
+    items = []
+    while (True):
+        # data = open("html/" + str(page) + ".html", "r", encoding="utf-8").read()
+        url = url + "/" + str(page)
+        data = web_requests(url)
+        if (data == None):
+            break
+
+        items = items + album_parse_html(data)
+
+        # 判断是否是最后一页
+        soup = BeautifulSoup(data, "html.parser")
+        total_num = int(soup.find("div", class_="pagination-page-info").find_all("b")[1].contents[0])
+        cur_num = int(soup.find("div", class_="pagination-page-info").find_all("b")[0].contents[0].replace(" ", "").split("-")[1])
+        if (cur_num >= total_num):
+            break
+        else:
+            page = page + 1
+    return items
+
+def album_cal_matching_rate(items):
+    rate = 1
+    return rate
+
+
+def album_save_items(items):
+    # 读取本地items
+    local_info = read_csv(web_info_path)
+    all_id = [] 
+    for a in local_info:
+        all_id.append(a[0])
+    
+    # 新增的items
+    new_info = []
+    for item in items:
+        if (item[0] not in all_id):
+            new_info.append(item)
+    
+    # 保存到本地
+    write_csv(web_info_path, new_info, "a+")
 
 """
 抓取共享列表url中的video page
 """
 def get_video_page():
     shared_playlists = get_shared_playlists()
-    for playlist in shared_playlists:
-        page = 1
-        while (True):
-            # data = open("html/" + str(page) + ".html", "r", encoding="utf-8").read()
-            url = playlist + "/" + str(page)
-            data = web_requests(url)
-            if (data == None):
-                break
-            parse_html(data)
-            soup = BeautifulSoup(data, "html.parser")
-            total_num = int(soup.find("div", class_="pagination-page-info").find_all("b")[1].contents[0])
-            cur_num = int(soup.find("div", class_="pagination-page-info").find_all("b")[0].contents[0].replace(" ", "").split("-")[1])
-            if (cur_num >= total_num):
-                break
-            else:
-                page = page + 1
-                time.sleep(random.randint(0, 2))
+    for playlist_url in shared_playlists:
+
+        # 获取当前播放列表所有条目信息
+        items = album_get_all_items(playlist_url)
+
+        # 分析该播放列表匹配度（根据favarite匹配）
+        rate = album_cal_matching_rate(items)
+
+        # 保存播放列表（去重）/丢掉播放列表
+        if (rate > 0):
+            album_save_items(items)
 
 
 def main_app():
